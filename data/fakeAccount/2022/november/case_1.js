f3f5cd62fb5d711bc52f4435f0e2eb5d2cdcb7ce
Pruthviraj <livefactchecker@protonmail.com>
2022-11-28 18:20:11 +0530

    deleted some files

--- data/fakeAccount/2022/november/case_1.js
+++
@@ -1,3 +0,0 @@
-const data = "<h1>Fake Twitter Account By @MahiRajput_ </h1>" + 
-"<p> This Account <a href='https://twitter.com/mahiRajput_'>@MahiRajput_</a> is a Fake Twitter Account" + 
-"that is Promoting Love Jihad on Twitter by tweeting idiotic tweets. she asked her users for she could marry to Muslim boy. </p>";
